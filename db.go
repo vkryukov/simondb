7da2eb1a0d3b121ad5c4e7bd9f46e2fc4b1a2581
Victor Kryukov <vkryukov@fastmail.com>
2016-12-19 19:44:43 -0800

    Perform initial import

--- db.go
+++ db.go
@@ -11,6 +11,8 @@ import (
 var _DB *ql.DB // Main database
 const dbName = "./movies.db"
 
+var insertMovie, insertActor, insertMovieActor ql.List
+
 func mustCreateTable(v interface{}) {
 	schema, err := ql.Schema(v, "", nil)
 	if err != nil {
@@ -31,4 +33,101 @@ func init() {
 	mustCreateTable(&Movie{})
 	mustCreateTable(&Actor{})
 	mustCreateTable(&MovieActor{})
+
+	insertMovie = ql.MustCompile("insert into Movie values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);")
+	insertActor = ql.MustCompile("insert into Actor values ($1, $2, $3, $4, $5);")
+	insertMovieActor = ql.MustCompile("insert into MovieActor values ($1, $2);")
+
+}
+
+// SaveToDB save movies information to the database
+func (movies Movies) SaveToDB() error {
+	ctx := ql.NewRWCtx()
+	if _, _, err := _DB.Run(ctx, "BEGIN TRANSACTION;"); err != nil {
+		return err
+	}
+	for _, m := range movies {
+		if _, _, err := _DB.Execute(
+			ctx, insertMovie,
+			m.ID, m.Title, m.Studio, m.Year, m.Duration, m.Kassette, m.DVD,
+			m.IMDBID, m.IMDBTitle, m.IMDBStudio, m.IMDBYear, m.IMDBDuration, m.Updated,
+		); err != nil {
+			return err
+		}
+	}
+	if _, _, err := _DB.Run(ctx, "COMMIT;"); err != nil {
+		return err
+	}
+	if err := _DB.Flush(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// SaveToDB save actors information to the database
+func (actors Actors) SaveToDB() error {
+	ctx := ql.NewRWCtx()
+	if _, _, err := _DB.Run(ctx, "BEGIN TRANSACTION;"); err != nil {
+		return err
+	}
+	for _, a := range actors {
+		if _, _, err := _DB.Execute(
+			ctx, insertActor,
+			a.ID, a.Name, a.IMDBID, a.IMDBName, a.Updated,
+		); err != nil {
+			return err
+		}
+	}
+	if _, _, err := _DB.Run(ctx, "COMMIT;"); err != nil {
+		return err
+	}
+	if err := _DB.Flush(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// SaveToDB save actors information to the database
+func (moviesActors MoviesActors) SaveToDB() error {
+	ctx := ql.NewRWCtx()
+	if _, _, err := _DB.Run(ctx, "BEGIN TRANSACTION;"); err != nil {
+		return err
+	}
+	for _, ma := range moviesActors {
+		if _, _, err := _DB.Execute(
+			ctx, insertMovieActor,
+			ma.MovieID, ma.ActorID,
+		); err != nil {
+			return err
+		}
+	}
+	if _, _, err := _DB.Run(ctx, "COMMIT;"); err != nil {
+		return err
+	}
+	if err := _DB.Flush(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func initialImportExport() {
+	movies, actors, moviesActors, err := ParseOriginalDB(originalDBName)
+	if err != nil {
+		log.Fatalf("Error parsing original file: %s\n", err)
+	}
+	if err = movies.WriteCSV("movies.csv"); err != nil {
+		log.Fatalf("Cannot output movies.csv: %s\n", err)
+	}
+	if err = actors.WriteCSV("actors.csv"); err != nil {
+		log.Fatalf("Cannot output actors.csv: %s\n", err)
+	}
+	if err = movies.SaveToDB(); err != nil {
+		log.Fatalf("Cannot write movies to DB: %s\n", err)
+	}
+	if err = actors.SaveToDB(); err != nil {
+		log.Fatalf("Cannot write actors to DB: %s\n", err)
+	}
+	if err = moviesActors.SaveToDB(); err != nil {
+		log.Fatalf("Cannot write moviesActors to DB: %s\n", err)
+	}
 }
--- main.go
+++ main.go
@@ -4,14 +4,5 @@ const originalDBName = "./simondb.csv"
 
 func main() {
 	defer _DB.Close()
-	movies, actors, _, err := ParseOriginalDB(originalDBName)
-	if err != nil {
-		panic(err)
-	}
-	if err = movies.WriteCSV("movies.csv"); err != nil {
-		panic(err)
-	}
-	if err = actors.WriteCSV("actors.csv"); err != nil {
-		panic(err)
-	}
+	initialImportExport()
 }
